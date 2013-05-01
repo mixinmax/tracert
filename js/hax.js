4a271eed1c6ca51c4888b4df8a6bb1c1ed65211c
Max Mackie <github@f33r.com>
2013-05-01 11:14:44 -0300

    Update hax.js

--- js/hax.js
+++ js/hax.js
@@ -69,4 +69,4 @@ function typeLetter(msg, pos) {
     }
 }
 
-var typeInterval = setInterval(function() { typeLetter(message, typeCount) }, 1);
+var typeInterval = setInterval(function() { typeLetter(message, typeCount) }, 30);
