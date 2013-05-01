838238d46c860833df96a3de401049d201eb20e7
Max Mackie <max@f33r.com>
2013-04-30 22:22:57 -0400

    first fully working example

--- js/hax.js
+++ js/hax.js
@@ -1,39 +1,76 @@
-var haxConsole = {
-    flicker: function() {
-        $("#hacker-interface-blur").animate({opacity: 0.3}, 50, function() {
-            $(this).animate({opacity:0.2}, 50, function() {
-                console.log("done");
-            });
-        });
-    },
-}
+var typeCount = 0;
+var tracertEngage = 0;
 
-var count = 0;
 var message = "Welcome Stranger... ******* %%";
 message += "You have been granted permission to use the single most powerful hacker tool in the history %";
 message += "of hacker tools: ***** tracert (pronounced 'tracer-tee'). ***** With great power, comes great responsibility.%%*****";
 message += "When you are ready to invoke the powers of tracert, invoke the command in the prompt below.%%";
 message += "haxor@hack-server:/ $  ";
 
-function killInterval() {
-    clearInterval(interval);
+var tracert = "%%";
+tracert += "Tracing route to fbi.gov [72.21.81.85]... ** %%";
+tracert += "1   ********1   ********1   ********1       ********70.84.211.97    61.d3.5446.static.theplanet.com **%";
+tracert += "2   ****0   ****0   ****0       ****70.87.254.1 po101.dsr01.dllstx5.networklayer.com **%";
+tracert += "3   ********1   ****0   ****0       ****70.85.127.105   po51.dsr01.dllstx3.networklayer.com **%";
+tracert += "4   ****0   ****0   ****0       ****173.192.18.224  ae16.bbr01.eq01.dal03.networklayer.com **%";       
+tracert += "6   **0   **0   **0       *****72.21.81.85 **%%";
+tracert += "Hacking complete. You've successfully infiltrated the FBI servers. You hacker, you.";
+
+function blinkCursor() {
 }
 
-function appendLinebreak() {
-    $("#content").append("<br>");
+function reddify() {
+    $("#hacker-interface-blur").animate({opacity: 0.2}, 50, function() {
+        $(".haxxx").fadeIn(200);
+        $(this).animate({opacity:0}, 50, function() { 
+            $(".haxxx").fadeOut(200);
+        });
+    });
+}
+
+function killInterval() {
+    clearInterval(typeInterval);
+    $("#content").append("<span id='command'></span>");
+    blinkCursor();
+    $('body').bind('keydown', function(e) {
+        e.preventDefault();
+        var key = e.keyCode;
+        if (key == 13) { 
+            if ($("#command").text() == 'tracert') {
+                reddify();
+                var omgHax = setInterval(function() { reddify(); }, 500);
+                typeCount = 0; 
+                tracertEngage = 1;
+                var doTheT = setInterval(function() { typeLetter(tracert, typeCount); }, 30);
+            } else {
+                console.log("aint no posts foo");
+            }
+        } else if (key == 8) { 
+            var contents = $("#command").text();
+            $("#command").text(contents.substring(0,(contents.length - 1)));
+        } else {
+            $("#command").append(String.fromCharCode(key).toLowerCase());
+        }
+    });
 }
 
 function typeLetter(msg, pos) {
     var letter = msg.charAt(pos);
     if (letter == "%") { 
-        appendLinebreak(); 
+        $("#content").append("<br>");
     } else if (letter == "*") {
         // do nothing
     } else {
         $("#content").append(letter);
     }
-    count++;
-    if (count == msg.length) { killInterval(); }
+    typeCount++;
+    if (typeCount == msg.length) { 
+        if (tracertEngage == 1) {
+            clearInterval(doTheT);
+            return;
+        }
+        killInterval(); 
+    }
 }
 
-var interval = setInterval(function() { typeLetter(message, count) }, 60);
+var typeInterval = setInterval(function() { typeLetter(message, typeCount) }, 30);
